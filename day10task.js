2b57de0c78b02594f44dd228203539878d423454
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2024-10-02 16:57:37 +0530

    Add files via upload

---
+++ day10task.js
@@ -0,0 +1,82 @@
+var day = 4;
+switch (day) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid day");
+}
+var month = 7;
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+  default:
+    console.log("Invalid month");
+}
+//task3
+var n = 1;
+while (n <= 10) {
+  if (n % 2 == 0) {
+    console.log(n + "evennumbers");
+  }
+  n++;
+}
+//task4
+let countdown = 10;
+while (countdown >= 0) {
+  console.log(countdown);
+  countdown--;
+}
+console.log("Countdown finished!");
